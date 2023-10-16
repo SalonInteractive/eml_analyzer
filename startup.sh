4e2ed7fc2ca38ebdc1a3fc71abbc9c55f7aef4d9
ggo <ggo@saloninteractive.com>
2023-10-16 10:27:22 -0500

    remove space in startup.sh?

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 cd spamassassin-dqs/4.0.0+ \
       && sed -i -e "s/your_DQS_key/$SPAMHAUS_DQS_KEY/g" sh.cf \
       && sed -i -e "s/your_DQS_key/$SPAMHAUS_DQS_KEY/g" sh_hbl.cf \
-      && sed -i -e "s/<config_directory>/\/etc\/mail\/spamassassin/g" sh.pre \ 
+      && sed -i -e "s/<config_directory>/\/etc\/mail\/spamassassin/g" sh.pre \
       && cp sh.cf /etc/mail/spamassassin \
       && cp sh_scores.cf /etc/mail/spamassassin \
       && cp sh_hbl.cf /etc/mail/spamassassin \
